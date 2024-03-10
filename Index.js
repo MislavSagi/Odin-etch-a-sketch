525424dcf34bc33af24dc2c1e48c366e60207a6a
Mislav Sagi <mislavshagi@gmail.com>
2024-03-10 20:40:19 +0100

    new css added

---
+++ Index.js
@@ -0,0 +1,10 @@
+let div = document.createElement('div');
+div.style.width = '100px';
+div.style.height = '100px';
+div.style.background = 'black';
+
+const addDiv = document.getElementById("container").appendChild(div);
+
+
+
+
